14c4262612907c0df02adc504cca1d3f07580048
jsphmglbzrdev <jsphmglbzr.dev@gmail.com>
2025-06-14 17:00:04 +0800

    Random quote generator
    This is a basic project using HTML, CSS, and Javascript. And a JSON file for fetching different quotes.

---
+++ random-quote-generator/script.js
@@ -0,0 +1,21 @@
+
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const button = document.getElementById('new-quote');
+
+const getQuote = async() => {
+
+    try{
+        const response = await fetch('./quotes.json');
+        const data = await response.json();
+        console.log(data)
+
+        const randomIndex = Math.floor(Math.random() * data.length);
+        quote.textContent = data[randomIndex].quote;
+        author.textContent = '- ' + data[randomIndex].author;
+    } catch (error) {
+        console.error('Error fetching quote:', error);
+    }
+
+}
+button.addEventListener('click', getQuote);
